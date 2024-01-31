469257cb4ebea8eaceb026f453bcb6db520228da
Saika <saikaz@proton.me>
2024-01-31 15:44:18 -0800

    QOL, end test mode

--- dataproc.m
+++ dataproc.m
@@ -1,17 +1,22 @@
-% Filename = input('Please input the file name:')
-Filename = 'RH001_Li4Ti5O12_initialtest_1C-2C-10C_2pt7V_CF7.txt'
+Filename = input('Please input the file name:',"s");
+if string(Filename(max(strlength(Filename)-3,1):strlength(Filename))) ~= ".txt"
+    Filename = Filename + ".txt";
+end
+% Filename = 'RH001_Li4Ti5O12_initialtest_1C-2C-10C_2pt7V_CF7.txt' %Test File
 Rawdata = readtable(Filename);
 Rawdata.Properties.VariableNames = ["cycle number" , "ox/red" , "control changes" , "Ns changes" , "time/s" , "step time/s" , "Ecell/V" , "<I>/mA" , "Capacity/mA.h" , "Q discharge/mA.h" , "Q charge/mA.h" , "dq/mA.h"];
 
-PeakVoltage = 2.7
-% PeakVoltage = input('Please input the peak (charged) voltage (±0.005V):');
+% PeakVoltage = 2.7 %Test voltage
+PeakVoltage = input('Please input the peak (charged) voltage (±0.005V):');
 
 nCycle = max(Rawdata{:,'cycle number'});
 lightBLUE = [0.356862745098039,0.811764705882353,0.956862745098039];
 darkBLUE = [0.0196078431372549,0.0745098039215686,0.670588235294118];
 blueGRADIENTflexible = @(i,N) lightBLUE + (darkBLUE-lightBLUE)*((i)/(N));
 
-figure
+vt = figure;
+vi = figure;
+qt = figure;
 Index = 1;
 LastState = -1; % 1 for charge, 0 for discharge
 for Cyclenum = (0: nCycle)
@@ -20,10 +25,21 @@ for Cyclenum = (0: nCycle)
         if (State ~= LastState) || (Index == height(Rawdata));
             if Index ~= 1
                 % Dataarray
+                figure(vt);
                 hold on
                 subplot(2,1,(2-LastState)) % subplot 1 for charge, 2 for discharge
                 plot(Dataarray(:,1),Dataarray(:,2),'color',blueGRADIENTflexible(Cyclenum,nCycle));
                 xlabel("time (s)");ylabel("Voltage (V)");
+                figure(vi);
+                hold on
+                subplot(2,1,(2-LastState)) % subplot 1 for charge, 2 for discharge
+                plot(Dataarray(:,2),Dataarray(:,3),'color',blueGRADIENTflexible(Cyclenum,nCycle));
+                xlabel("Voltage (V)");ylabel("Current (mA)");
+                figure(qt);
+                hold on
+                subplot(2,1,(2-LastState)) % subplot 1 for charge, 2 for discharge
+                plot(Dataarray(:,1),Dataarray(:,4),'color',blueGRADIENTflexible(Cyclenum,nCycle));
+                xlabel("time (s)");ylabel("Capacity (mAh)");
             end
             if (Index == height(Rawdata))
                 break
@@ -46,5 +62,7 @@ for Cyclenum = (0: nCycle)
 end
 hold off
 
-exportgraphics(gcf,'chart.png','Resolution',300)
+exportgraphics(vt,'TimeVoltage.png','Resolution',300)
+exportgraphics(vi,'VoltageCurrent.png','Resolution',300)
+exportgraphics(qt,'TimeCapacity.png','Resolution',300)
 close
