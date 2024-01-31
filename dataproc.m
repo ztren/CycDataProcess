2c07297b59976445f01b199843cfd2956c266943
Saika <saikaz@proton.me>
2024-01-31 15:06:46 -0800

    Initial commit

---
+++ dataproc.m
@@ -0,0 +1,50 @@
+% Filename = input('Please input the file name:')
+Filename = 'RH001_Li4Ti5O12_initialtest_1C-2C-10C_2pt7V_CF7.txt'
+Rawdata = readtable(Filename);
+Rawdata.Properties.VariableNames = ["cycle number" , "ox/red" , "control changes" , "Ns changes" , "time/s" , "step time/s" , "Ecell/V" , "<I>/mA" , "Capacity/mA.h" , "Q discharge/mA.h" , "Q charge/mA.h" , "dq/mA.h"];
+
+PeakVoltage = 2.7
+% PeakVoltage = input('Please input the peak (charged) voltage (±0.005V):');
+
+nCycle = max(Rawdata{:,'cycle number'});
+lightBLUE = [0.356862745098039,0.811764705882353,0.956862745098039];
+darkBLUE = [0.0196078431372549,0.0745098039215686,0.670588235294118];
+blueGRADIENTflexible = @(i,N) lightBLUE + (darkBLUE-lightBLUE)*((i)/(N));
+
+figure
+Index = 1;
+LastState = -1; % 1 for charge, 0 for discharge
+for Cyclenum = (0: nCycle)
+    while Rawdata{Index, "cycle number"} == Cyclenum
+        State = Rawdata{Index, "ox/red"};
+        if (State ~= LastState) || (Index == height(Rawdata));
+            if Index ~= 1
+                % Dataarray
+                hold on
+                subplot(2,1,(2-LastState)) % subplot 1 for charge, 2 for discharge
+                plot(Dataarray(:,1),Dataarray(:,2),'color',blueGRADIENTflexible(Cyclenum,nCycle));
+                xlabel("time (s)");ylabel("Voltage (V)");
+            end
+            if (Index == height(Rawdata))
+                break
+            end
+            Dataarray = []; % Time, Voltage, Current, Capacity
+            t0 = Rawdata{Index,"time/s"}; %Time when charge/discharge starts
+        end
+        h = height(Dataarray);
+        if (State ~= 1) || (h < 2) || ((abs(Dataarray(h,2)/Dataarray(h-1,2))-1) > 6E-5) || (abs(Dataarray(h,2) - PeakVoltage) > 0.005) % Trimming
+            Time = Rawdata{Index,"time/s"} - t0;
+            Voltage = Rawdata{Index,"Ecell/V"};
+            Current = Rawdata{Index, "<I>/mA"};
+            Capacity = Rawdata{Index, "Capacity/mA.h"};
+            Dataarray = [Dataarray; Time, Voltage, Current, Capacity];
+        end
+        
+        LastState = State;
+        Index = Index + 1;
+    end
+end
+hold off
+
+exportgraphics(gcf,'chart.png','Resolution',300)
+close
